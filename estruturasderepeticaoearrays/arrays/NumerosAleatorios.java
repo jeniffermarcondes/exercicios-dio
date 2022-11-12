543191d2384bcb5066df8cf8180214e7fff1345d
jeniffer.almas <jeniffer.almas@jallcard.com.br>
2022-11-12 10:19:01 -0300

    Numeros Aleatórios

---
+++ estruturasderepeticaoearrays/arrays/NumerosAleatorios.java
@@ -0,0 +1,37 @@
+package estruturasderepeticaoearrays.arrays;
+
+import java.util.Random;
+
+/*
+ Faça um programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+ Ao final mostre os números e seus sucessores.
+ */
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+        System.out.print("Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " ");
+        }
+
+        System.out.print("\nAntessores dos Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero - 1) + " ");
+        }
+
+        System.out.print("\nSucessores dos Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero + 1) + " ");
+        }
+
+    }
+
+}
